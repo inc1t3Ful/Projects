cf69e7d0c598f7a7558da3aecd7dcf97ccb0c1f3
Anthony <ant.lee813@gmail.com>
2016-08-06 15:43:05 -0700

    Added functionality to edit meals
    Can edit existing meals and correctly overwrite pre-existing meal data rather than save a new cell

--- Food Tracker/Food Tracker/MealTableViewController.swift
+++ Food Tracker/Food Tracker/MealTableViewController.swift
@@ -104,24 +104,50 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetail" {
+            
+            let mealDetailViewController = segue.destinationViewController as! MealViewController
+            
+            // Get the cell that generated this segue.
+            if let selectedMealCell = sender as? MealTableViewCell {
+                
+                let indexPath = tableView.indexPathForCell(selectedMealCell)!
+                let selectedMeal = meals[indexPath.row]
+                mealDetailViewController.meal = selectedMeal
+            }
+        }
+        else if segue.identifier == "AddItem" {
+            
+            print("Adding new meal.")
+        }
     }
-    */
+    
     
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         
         if let sourceViewController = sender.sourceViewController as? MealViewController, meal = sourceViewController.meal {
             
-            // Add a new meal.
-            let newIndexPath = NSIndexPath(forRow: meals.count, inSection: 0)
-            meals.append(meal)
-            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                
+                // Update an existing meal.
+                // The first line updates the appropriate entry in meals to store the updated meal information. 
+                // The second line reloads the appropriate row in the table view to display the changed data.
+                
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRowsAtIndexPaths([selectedIndexPath], withRowAnimation: .None)
+            }
+            
+            else {
+                // Add a new meal.
+                let newIndexPath = NSIndexPath(forRow: meals.count, inSection: 0)
+                meals.append(meal)
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+            }
         }
     }
 
