b3c4062e464ff70c2a013ebf83505300aadcd085
Anthony Lee <ant.lee813@gmail.com>
2018-09-09 00:02:30 -0700

    fixed day/month edge cases
    ** will no longer have cases such as Feb 31st or Nov 31st

--- IdentityGenerator.py
+++ IdentityGenerator.py
@@ -18,7 +18,12 @@ email = addresses[random.randint(0, len(addresses)-1)]
 # birthday function
 year = str(random.randint(1990, 2001))
 month = str(random.randint(1, 12))
-day = str(random.randint(1, 31))
+if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+    day = str(random.randint(1, 31))
+elif(month == 4 or month == 6 or month == 9 or month == 11):
+    day = str(random.randint(1, 30))
+else:
+    day = str(random.randint(1, 28))
 
 print(" ")
 print("Gender: " + gender)
