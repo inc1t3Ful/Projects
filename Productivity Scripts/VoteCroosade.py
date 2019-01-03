2bb17684fe70800ba6390d492d0d0b29bfee8059
inc1t3Ful <ant.lee813@gmail.com>
2019-01-02 21:14:50 -0800

    VoteCroosade 2.0 Update
    ++ Added automatic looping for 6hr intervals
    ++ Added usage documentation
    ** cleaned up code
    ** refactored code blocks for sleep

--- Productivity Scripts/VoteCroosade.py
+++ Productivity Scripts/VoteCroosade.py
@@ -1,10 +1,39 @@
 """
 VoteCroosade.py by Anthony Lee
-v1.0
-Last updated: 26 Dec 2018
+v2.0
+Last updated: 02 Jan 2019
 
-This script serves to automate the voting process for CroosadeMS v100, a
-Maplestory private server
+#########################################################################
+This script automates the voting process for CroosadeMS v100, a
+Maplestory private server, and will automatically vote again every
+reset period, which is 6 hours.
+
+It leverages the site's lack of vote verification from the 3rd-party sites
+and automates navigation to the main site, login, and the voting site
+selections, avoiding all the CAPTCHAs.
+
+#########################################################################
+USAGE: ($ represents command-line arg)
+
+1. If you do not have Selenium already installed, please run:
+    $ pip install selenium
+
+2. If you do not have Selenium Chrome Driver installed:
+    https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+3. Extract 'chromedriver.exe' and place in any directory, preferably in the
+same directory as this script
+
+4. Note directory path and update it accordingly in this script as denoted on
+line 61. DO NOT remove the quotation marks ("")
+
+5. Fill in login credentials as denoted on lines 64-65. DO NOT remove the
+quotation marks ("")
+
+6. Assuming you are in the same directory as your script, to run script:
+    $ python VoteCroosade.py
+
+#########################################################################
 """
 # import webbrowser as wb
 from selenium import webdriver
@@ -13,115 +42,104 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
-import logging
 
-# Note: Place script in same directory as ChromeWebdriver or vice versa;
-# Edit the path as necessary
-driver = webdriver.Chrome("/filepath/chromedriver")
+starttime = time.time()
 
-# Note: Input your login credentials here
-username = "username"
-password = "password"
+# Repetitive block to buffer page loading
+# USAGE: x = seconds to sleep
+def pageLoadSleep(x):
+    print("")
+    print("Ensuring page loads...")
+    print("")
+    time.sleep(x)
 
-# driver.switch_to_window(window_name) # need to find window name
-# using driver.get() refreshes the page instance, removing logins
-# print(driver.assertTrue("https://www.croosade.com/vote", currURL)) # assert is not working atm
+# This locks loop to system clock; guaranteed run every x interval
+# NOTE: sleep is in seconds (21660 seconds = 6 HR 1 MIN; Script runs ~30 SEC)
+while True:
+    # NOTE: Place script in same directory as ChromeWebdriver or vice versa;
+    # NOTE: EDIT PATH TO CHROMEDRIVER.EXE ON LINE BELOW
+    driver = webdriver.Chrome("/filepath/chromedriver")
 
-## Navigate to Croosade main site
-print("")
-print(">> Navigating to Croosade site")
-driver.get("https://croosade.com/")
-print("")
-print("Ensuring page loads...")
-print("")
-time.sleep(1)
+    # NOTE: INPUT LOGIN CREDENTIALS BELOW
+    username = "username"
+    password = "password"
 
-## Access drop down menu and click login
-print(">> Clicking Drop down menu")
-dropDownAccountButton = driver.find_element_by_link_text("Account")
-dropDownAccountButton.click()
-print(">> Clicking Sign In")
-signInButton = driver.find_element_by_link_text("Sign in")
-signInButton.click()
+    ## Navigate to Croosade main site
+    print("")
+    print(">> Navigating to Croosade site")
+    driver.get("https://croosade.com/")
+    pageLoadSleep(1)
 
-print("")
-print("Ensuring page loads...")
-print("")
-time.sleep(1)
+    ## Access drop down menu and click login
+    print(">> Clicking Drop down menu")
+    dropDownAccountButton = driver.find_element_by_link_text("Account")
+    dropDownAccountButton.click()
+    print(">> Clicking Sign In")
+    signInButton = driver.find_element_by_link_text("Sign in")
+    signInButton.click()
 
-## Enter login credentials
-print("Currently on: << " + driver.current_url + " >>")
-print(">> Inputting username and password")
-print(">> Username: " + username)
-print(">> Password: " + password)
-# Wait for login fields to be located before attempting to input
-usernameField = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, 'inputUsername')))
-passwordField = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, 'inputPassword')))
-usernameField.send_keys(username)
-passwordField.send_keys(password)
-submitLogin = driver.find_element_by_css_selector("#formLogin > button")
-submitLogin.click()
-print("")
-print("Logging in...")
+    ## Enter login credentials
+    print("")
+    print("Currently on: << " + driver.current_url + " >>")
+    print(">> Inputting username and password")
+    print(">> Username: " + username)
+    print(">> Password: " + password)
+    # Wait for login fields to be located before attempting to input
+    usernameField = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'inputUsername')))
+    passwordField = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'inputPassword')))
+    usernameField.send_keys(username)
+    passwordField.send_keys(password)
+    submitLogin = driver.find_element_by_css_selector("#formLogin > button")
+    submitLogin.click()
+    print("")
+    print("Logging in...")
+    pageLoadSleep(1)
 
-print("")
-print("Ensuring page loads...")
-print("")
-time.sleep(2)
+    ## On Dashboard panel, go to voting panel
+    print("Currently on: << " + driver.current_url + " >>")
+    print(">> Clicking Vote for NX")
+    voteForNX = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/a")
+    voteForNX.click()
+    pageLoadSleep(1)
 
-## On Dashboard panel, go to voting panel
-print("Currently on: << " + driver.current_url + " >>")
-print(">> Clicking Vote for NX")
-voteForNX = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/a")
-voteForNX.click()
+    ## Vote for Gtop100
+    # by default, Gtop100 is already selected ; do not need to select dropdown menu
+    print("Currently on: << " + driver.current_url + " >>")
+    print(">> Voting for Gtop100")
+    voteButton = driver.find_element_by_css_selector("#formVote > button")
+    voteButton.click()
+    driver.switch_to_window(driver.window_handles[0])
+    time.sleep(1)
 
-print("")
-print("Ensuring page loads...")
-print("")
-time.sleep(2)
+    ## Vote for XtremeTop100
+    print(">> Voting for XtremeTop100")
+    selectTopList = driver.find_element_by_id("selectToplist")
+    selectOption2 = driver.find_element_by_css_selector("#selectToplist > option:nth-child(2)")
+    selectTopList.click()
+    selectOption2.click()
+    voteButton.click()
+    driver.switch_to_window(driver.window_handles[0])
+    time.sleep(1)
 
-## Vote for Gtop100
-# by default, Gtop100 is already selected ; do not need to select dropdown menu
-print("Currently on: << " + driver.current_url + " >>")
-print(">> Voting for Gtop100")
-voteButton = driver.find_element_by_css_selector("#formVote > button")
-voteButton.click()
-# print("Currently on: << " + driver.current_url + " >>")
-driver.switch_to_window(driver.window_handles[0])
-# print("Currently on: << " + driver.current_url + " >>")
-time.sleep(1)
+    ## Vote for TopG
+    print(">> Voting for TopG")
+    selectOption3 = driver.find_element_by_css_selector("#selectToplist > option:nth-child(3)")
+    selectTopList.click()
+    selectOption3.click()
+    voteButton.click()
+    driver.switch_to_window(driver.window_handles[0])
+    pageLoadSleep(2)
 
-## Vote for XtremeTop100
-print(">> Voting for XtremeTop100")
-selectTopList = driver.find_element_by_id("selectToplist")
-selectOption2 = driver.find_element_by_css_selector("#selectToplist > option:nth-child(2)")
-selectTopList.click()
-selectOption2.click()
-time.sleep(1)
-voteButton.click()
-# print("Currently on: << " + driver.current_url + " >>")
-driver.switch_to_window(driver.window_handles[0])
-# print("Currently on: << " + driver.current_url + " >>")
-time.sleep(1)
+    print(">> Closing window")
+    driver.quit() # this closes the entire window/ driver instance
 
-## Vote for TopG
-print(">> Voting for TopG")
-selectOption3 = driver.find_element_by_css_selector("#selectToplist > option:nth-child(3)")
-selectTopList.click()
-selectOption3.click()
-time.sleep(1)
-voteButton.click()
-# print("Currently on: << " + driver.current_url + " >>")
-driver.switch_to_window(driver.window_handles[0])
-# print("Currently on: << " + driver.current_url + " >>")
-
-print("")
-print("Ensuring page loads...")
-print("")
-time.sleep(3)
-# print("Closing tab...")
-# driver.close()
-print("Closing window...")
-driver.quit() # this closes the entire window
+    ## Loop block
+    print("")
+    print("Current time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())))
+    print(">> Script will automatically rerun in 6 hours")
+    print("")
+    print("Please do not close your Command Prompt/ Terminal")
+    print("To stop the program at any time, press CTRL + C")
+    time.sleep(21660.0 - ((time.time() - starttime) % 21660.0)) # DO NOT CHANGE TIME
